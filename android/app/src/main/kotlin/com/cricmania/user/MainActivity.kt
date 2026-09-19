615dd6f54f3681642e2a06f99da0fea19a1d6b17
WadoodSanjrani <wadoodkhan007@gmail.com>
2026-09-20 03:37:49 +0500

    User App - Initial Commit

---
+++ android/app/src/main/kotlin/com/cricmania/user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cricmania.user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cric Mania',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cric Mania'),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Cric Mania!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
